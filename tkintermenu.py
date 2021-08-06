a1f1e521c4991cf2789a3131c9739d50ed748e51
Vladimir Dyuzhy <87826714+DelAIH@users.noreply.github.com>
2021-08-06 10:00:41 +0300

    Tkinter 0.2
    Modified the interface a bit

--- tkintermenu.py
+++ tkintermenu.py
@@ -1,37 +1,22 @@
 from tkinter import*
 
 window = Tk()
-window.geometry('300x150')
-
+window.geometry('268x70')
+window.title('DelAIH')
 def clicked():
 	name = ent.get()
-	ent1.insert(0, name)
+	ent2.insert(0, name)
   
-  
-lbl = Label(
-	text="Пароль",
-	width=20
-	)
+lbl = Label(window, text="Пароль:", width=8)
+ent = Entry(window, width = 20)
+btn = Button(window, text = 'Ввод',width = 10,command = clicked, fg='white', bg='black')
+ent2 = Entry(window, width = 20)
+lbl2 = Label(window, text="Результат:", width=8)
 
-ent = Entry(
-	width=20
-	)
-
-
-	
-btn = Button(
-	text='Ввод',
-	width=20,
-	command = clicked
-	)
-
-ent1 = Entry(
-	width=20
-	)
-
-lbl.pack()
-ent.pack()
-btn.pack()
-ent1.pack()
+lbl.grid(row=0, column=0)
+ent.grid(row=0, column=1)
+btn.grid(row=0, column=2)
+ent2.grid(row=1, column=1)
+lbl2.grid(row=1, column=0)
 
 window.mainloop()
